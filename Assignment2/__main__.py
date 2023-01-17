ddf1d9c2bd224cc9b2c5d1db4b3ec09fe755d43f
DirkKeller <70960084+DirkKeller@users.noreply.github.com>
2023-01-17 18:01:47 +0100

    Ready for the replication experiment

---
+++ Assignment2/__main__.py
@@ -0,0 +1,288 @@
+"""
+---
+title: Denoising Diffusion Probabilistic Models (DDPM) training
+summary: >
+  Training code for
+  Denoising Diffusion Probabilistic Model.
+---
+
+# [Denoising Diffusion Probabilistic Models (DDPM)](index.html) training
+
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/diffusion/ddpm/experiment.ipynb)
+
+This trains a DDPM based model on CelebA HQ dataset. You can find the download instruction in this
+[discussion on fast.ai](https://forums.fast.ai/t/download-celeba-hq-dataset/45873/3).
+Save the images inside [`data/celebA` folder](#dataset_path).
+
+The paper had used a exponential moving average of the model with a decay of $0.9999$. We have skipped this for
+simplicity.
+
+(obtained from: From: https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/diffusion/ddpm/experiment.py)
+"""
+
+from typing import List, Tuple
+
+import torch
+import torch.utils.data
+import torchvision
+from PIL import Image
+
+from labml import lab, tracker, experiment, monit
+from labml.configs import BaseConfigs, option
+from labml_helpers.device import DeviceConfigs
+from noise import DenoiseDiffusion
+from unet import UNet
+
+def main():
+    # Create experiment
+    experiment.create(name='diffuse', writers={'screen', 'labml'})
+
+    # Create configurations
+    configs = Configs()
+
+    # Set configurations. You can override the defaults by passing the values in the dictionary.
+    experiment.configs(configs, {
+        'dataset': 'MNIST',  # 'CelebA'
+        'image_channels': 1,  # 3,
+        'epochs': 5,  # 100,
+    })
+
+    # Initialize
+    configs.init()
+
+    # Set models for saving and loading
+    experiment.add_pytorch_models({'eps_model': configs.eps_model})
+
+    # Start and run the training loop
+    with experiment.start():
+        configs.run()
+
+
+class Configs(BaseConfigs):
+    """
+    Class for holding configuration parameters for training a DDPM model.
+
+    Attributes:
+        device (torch.device):           Device on which to run the model.
+        eps_model (UNet):                U-Net model for the function `epsilon_theta`.
+        diffusion (DenoiseDiffusion):    DDPM algorithm.
+        image_channels (int):            Number of channels in the image (e.g. 3 for RGB).
+        image_size (int):                Size of the image.
+        n_channels (int):                Number of channels in the initial feature map.
+        channel_multipliers (List[int]): Number of channels at each resolution.
+        is_attention (List[bool]):       Indicates whether to use attention at each resolution.
+        n_steps (int):                   Number of time steps.
+        batch_size (int):                Batch size.
+        n_samples (int):                 Number of samples to generate.
+        learning_rate (float):           Learning rate.
+        epochs (int):                    Number of training epochs.
+        dataset (torch.utils.data.Dataset):         Dataset to be used for training.
+        data_loader (torch.utils.data.DataLoader):  DataLoader for loading the data for training.
+        optimizer (torch.optim.Adam):               Optimizer for the model.
+    """
+
+
+
+
+    # Device to train the model on.
+    # [`DeviceConfigs`](https://docs.labml.ai/api/helpers.html#labml_helpers.device.DeviceConfigs)
+    #  picks up an available CUDA device or defaults to CPU.
+    device: torch.device = DeviceConfigs()
+
+    # U-Net model for $\textcolor{lightgreen}{\epsilon_\theta}(x_t, t)$
+    eps_model: UNet
+    # [DDPM algorithm](index.html)
+    diffusion: DenoiseDiffusion
+
+    # Number of channels in the image. $3$ for RGB.
+    image_channels: int = 3
+    # Image size
+    image_size: int = 32
+    # Number of channels in the initial feature map
+    n_channels: int = 64
+    # The list of channel numbers at each resolution.
+    # The number of channels is `channel_multipliers[i] * n_channels`
+    channel_multipliers: List[int] = [1, 2, 2, 4]
+    # The list of booleans that indicate whether to use attention at each resolution
+    is_attention: List[int] = [False, False, False, True]
+
+    # Number of time steps $T$
+    n_steps: int = 1_000
+    # Batch size
+    batch_size: int = 64
+    # Number of samples to generate
+    n_samples: int = 16
+    # Learning rate
+    learning_rate: float = 2e-5
+
+    # Number of training epochs
+    epochs: int = 1_000
+
+    # Dataset
+    dataset: torch.utils.data.Dataset
+    # Dataloader
+    data_loader: torch.utils.data.DataLoader
+
+    # Adam optimizer
+    optimizer: torch.optim.Adam
+
+    def init(self):
+        """
+        Initialize the model, dataset, and optimizer objects.
+        """
+
+        # Create $\textcolor{lightgreen}{\epsilon_\theta}(x_t, t)$ model
+        self.eps_model = UNet(
+            image_channels=self.image_channels,
+            n_channels=self.n_channels,
+            ch_mults=self.channel_multipliers,
+            is_attn=self.is_attention,
+        ).to(self.device)
+
+        # Create [DDPM class](index.html)
+        self.diffusion = DenoiseDiffusion(
+            eps_model=self.eps_model,
+            n_steps=self.n_steps,
+            device=self.device,
+        )
+
+        # Create dataloader
+        self.data_loader = torch.utils.data.DataLoader(self.dataset, self.batch_size, shuffle=True, pin_memory=True)
+        # Create optimizer
+        self.optimizer = torch.optim.Adam(self.eps_model.parameters(), lr=self.learning_rate)
+
+        # Image logging
+        tracker.set_image("sample", True)
+
+    def sample(self) -> None:
+        """
+        Generate samples from a trained Denoising Diffusion Probabilistic Model (DDPM).
+        """
+
+        with torch.no_grad():
+            # Sample from the noise distribution at the final time step: x_T ~ p(x_T) = N(x_T; 0, I)
+            x = torch.randn([self.n_samples, self.image_channels, self.image_size, self.image_size],
+                            device=self.device)
+
+            # Remove noise at each time step in reverse order (so remove noise for T steps)
+            for t_ in monit.iterate('Sample', self.n_steps):
+                # Get current time step
+                t = self.n_steps - t_ - 1
+                # Sample from the noise distribution at the current time step: x_{t-1} ~ p_theta(x_{t-1}|x_t)
+                x = self.diffusion.p_sample(x, x.new_full((self.n_samples,), t, dtype=torch.long))
+
+            # Log the final denoised samples
+            tracker.save('sample', x)
+
+    def train(self) -> None:
+        """
+        Train a Denoising Diffusion Probabilistic Model (DDPM) with the set dataloader.
+        """
+        # Iterate through the dataset
+        for data in monit.iterate('Train', self.data_loader):
+            # Increment global step
+            tracker.add_global_step()
+            # Move data to device
+            data = data.to(self.device)
+
+            # Make the gradients zero
+            self.optimizer.zero_grad()
+            # Calculate loss
+            loss = self.diffusion.loss(data)
+            # Compute gradients
+            loss.backward()
+            # Take an optimization step
+            self.optimizer.step()
+            # Track the loss
+            tracker.save('loss', loss)
+
+    def run(self):
+        """
+        ### Training loop
+        """
+        for _ in monit.loop(self.epochs):
+            # Train the model
+            self.train()
+            # Sample some images
+            self.sample()
+            # New line in the console
+            tracker.new_line()
+            # Save the model
+            experiment.save_checkpoint()
+
+
+class CelebADataset(torch.utils.data.Dataset):
+    """
+    A PyTorch dataset for the CelebA-HQ dataset.
+    """
+
+    def __init__(self, image_size: int):
+        super().__init__()
+
+        # CelebA images folder
+        folder = lab.get_data_path() / 'celebA'
+        # List of files
+        self._files = [p for p in folder.glob(f'**/*.jpg')]
+
+        # Transformations to resize the image and convert to tensor
+        self._transform = torchvision.transforms.Compose([
+            torchvision.transforms.Resize(image_size),
+            torchvision.transforms.ToTensor(),
+        ])
+
+    def __len__(self) -> int:
+        """
+        Return the length of the dataset.
+        """
+        return len(self._files)
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Return the `index`-th image and its target image from the dataset.
+
+        Args:
+            index: Index of the image in the dataset.
+
+        Returns:
+            A tuple of the image tensor and the target image tensor.
+        """
+
+        img = Image.open(self._files[index])
+        return self._transform(img)
+
+
+@option(Configs.dataset, 'CelebA')
+def celeb_dataset(c: Configs):
+    """
+    Create CelebA dataset
+    """
+    return CelebADataset(c.image_size)
+
+
+class MNISTDataset(torchvision.datasets.MNIST):
+    """
+    ### MNIST dataset
+    """
+
+    def __init__(self, image_size):
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.Resize(image_size),
+            torchvision.transforms.ToTensor(),
+        ])
+
+        super().__init__(str(lab.get_data_path()), train=True, download=True, transform=transform)
+
+    def __getitem__(self, item):
+        return super().__getitem__(item)[0]
+
+
+@option(Configs.dataset, 'MNIST')
+def mnist_dataset(c: Configs):
+    """
+    Create MNIST dataset
+    """
+    return MNISTDataset(c.image_size)
+
+#
+if __name__ == '__main__':
+    main()
